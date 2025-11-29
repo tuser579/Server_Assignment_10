2f65cc3d34cfb74a6f21004728b7c4f76d45f39a
tuser579 <tusermon720@gmail.com>
2025-11-29 21:35:45 +0600

    booking side added

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const usersDB = client.db("usersDB");
     const usersCollection = usersDB.collection("users");
@@ -261,9 +261,9 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
